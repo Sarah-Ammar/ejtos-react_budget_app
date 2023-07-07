34f2f6a5f749b1f7635c5e89df80140f38188d2b
Sarah-Ammar <sarah.ammar94.sa90@gmail.com>
2023-07-07 10:25:33 -0400

    completed task 2.2, did not take a screenshot yet

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, ChangeEvent } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const [budget, setBudget] = useState(0);
+
+    const up = (value)=> {
+        console.log(`value is ${value}`);
+        if (value >= 20000) {
+            console.log("in alert block!")
+            window.alert(`The value of ${value} cannot exceed 20000`);
+        } 
+    }
+
+    const changeBudgetHandler = (event) => {
+        setBudget(event.target.value); 
+        up(event.target.value);
+    }
+
     return (
         <div className='alert alert-secondary'>
             <label for="budget">Budget: £</label>
-            <input id="budget1" type="number" name="budget" min="1000" max="20000"  step="1000"/>    
+            <input id="budget1" type="number" name="budget" min="1000" max="20000" step="1000"
+                onChange= {changeBudgetHandler}
+                />
         </div>
     );
 };
